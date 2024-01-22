4d8e125af8034fa33f93cb3231f53cf0acfdf205
Rao Kashif <mkrao455icloud.com@Raos-MacBook-Air.local>
2024-01-22 22:19:38 +0500

    Image optimization in NEXT

---
+++ app/photoUpload/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+export default function Image_Showing()
+{
+    return(
+        <div>The Image shown is as under: 
+        <Image
+        src="https://st.depositphotos.com/1144687/1476/i/950/depositphotos_14767997-stock-photo-social-media-concept.jpg?forcejpeg=true"/>
+</div>
+    )
+}
