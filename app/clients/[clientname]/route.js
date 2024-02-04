90d0a4e9788296d76e5fe1945b107de6694c7f8b
Rayan Sikandar <ryansikandar@gmail.com>
2024-02-04 16:53:40 +0500

    Worked with PUT API

--- app/clients/[clientname]/route.js
+++ app/clients/[clientname]/route.js
@@ -2,14 +2,29 @@ import { NextResponse } from "next/server";
 import { user } from "../../util/db";
 
 export function GET(request, content) {
-  console.log(content.params.clientname);
   const data = user;
 
   const userData = data.filter(
     (item) => item.name == content.params.clientname
   );
   return NextResponse.json(
-    userData.length == 0 ? { result: "No Data found" } : {result: userData},
-    {status: 200} 
+    userData.length == 0 ? { result: "No Data found" } : { result: userData },
+    { status: 200 }
   );
+}
+
+export async function PUT(req, content) {
+  let payload = await req.json();
+  console.log(payload)
+  console.log(content)
+  payload.name = content.params.clientname
+  console.log(payload)
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: "required field not found", success: false },
+      { status: 400 }
+    );
+  }
+  else
+  return NextResponse.json({result:payload,success:true},{status:200})
 }
--- app/clients/route.js
+++ app/clients/route.js
@@ -1,17 +1,21 @@
-import { NextResponse } from "next/server"
-import {user} from "../util/db"
+import { NextResponse } from "next/server";
+import { user } from "../util/db";
 
-export function GET(request)
-{
-const data = user
-return NextResponse.json(data,{status:200})
+export function GET(request) {
+  const data = user;
+  return NextResponse.json(data, { status: 200 });
 }
 
-export async function POST(request){
-    let payload = await request.json();
-    if(!payload.name || !payload.age || !payload.email)
-    {
-        return NextResponse.json({result:"rwquired field not found",success:false},{status:400})
-    }
-    return NextResponse.json({name: "New User Created",success:true},{status:201})
+export async function POST(request) {
+  let payload = await request.json();
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: "required field not found", success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { name: "New User Created", success: true },
+    { status: 201 }
+  );
 }
